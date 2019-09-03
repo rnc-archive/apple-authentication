2efb5f39f01cf3739fa0a4a223d02abd7664a8a9
thib92 <thibhenry92@gmail.com>
2019-09-04 00:08:32 +0200

    Add missing TS docs

--- index.ts
+++ index.ts
@@ -73,24 +73,57 @@ export interface SignInWithAppleCredential {
 }
 
 export interface SignInWithAppleScopes {
+  /**
+   * A scope that includes the user’s full name.
+   */
   FULL_NAME: string;
+  
+  /**
+   * A scope that includes the user’s email address.
+   */
   EMAIL: string;
 }
 
 export type SignInWithAppleScope = keyof SignInWithAppleScopes;
 
 export interface SignInWithAppleOperations {
+  /**
+   * An operation used to authenticate a user.
+   */
   LOGIN: string;
+
+  /**
+   * An operation that ends an authenticated session.
+   */
   LOGOUT: string;
+
+  /**
+   * An operation that refreshes the logged-in user’s credentials.
+   */
   REFRESH: string;
+
+  /**
+   * An operation that depends on the particular kind of credential provider.
+   */
   IMPLICIT: string;
 }
 
 export type SignInWithAppleOperation = keyof SignInWithAppleOperations;
 
 export interface SignInWithAppleUserDetectionStatuses {
+  /**
+   * The user appears to be a real person.
+   */
   LIKELY_REAL: string;
+
+  /**
+   * The system hasn’t determined whether the user might be a real person.
+   */
   UNKNOWN: string;
+
+  /**
+   * The system can’t determine this user’s status as a real person.
+   */
   UNSUPPORTED: string;
 }
 
@@ -98,9 +131,25 @@ export type SignInWithAppleUserDetectionStatus = keyof SignInWithAppleUserDetect
 
 
 export interface ISignInWithApple {
+  /**
+   * Perform a Sign In with Apple request with the given SignInWithAppleOptions.
+   * The method will return a Promise which will resolve to a SignInWithAppleCredential on success.
+   * You should make sure you include error handling.
+   */
   requestAsync: (signInWithAppleOptions: SignInWithAppleOptions) => Promise<SignInWithAppleCredential>;
+  /**
+   * Controls which scopes you are requesting when the call SignInWithApple.requestAsync().
+   */
   Scope: SignInWithAppleScopes;
+  /**
+   * Controls what operation you are requesting when the call SignInWithApple.requestAsync().
+   */
   Operation: SignInWithAppleOperations;
+  /**
+   * A value that indicates whether the user appears to be a real person.
+   * You get this in the realUserStatus property of a SignInWithAppleCredential object.
+   * It can be used as one metric to help prevent fraud.
+   */
   UserDetectionStatus: SignInWithAppleUserDetectionStatuses;
 }
 
